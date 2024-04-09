048c4b5dee5d82852e0151c22418390e5c2e6304
Ranvijay Patel <ranvijayid76@gmail.com>
2024-04-09 17:45:40 +0530

    db changes

--- config/db.py
+++ config/db.py
@@ -1,11 +1,10 @@
+import certifi
 from pymongo import MongoClient
 
 # MongoDB Connection and Database
-tls = True
-tlsAllowInvalidCertificates = True
 MONGO_URI = "mongodb+srv://ranvijay:ranvijay@cluster0.as3t4fm.mongodb.net/students"
 
-client = MongoClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 
 db = client["students"]
 collection = db["students"]
