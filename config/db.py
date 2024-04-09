d130d3f9373c85a7c9862a270d8e7f432ba5182f
Ranvijay Patel <ranvijayid76@gmail.com>
2024-04-09 18:16:24 +0530

    changed db.py

--- config/db.py
+++ config/db.py
@@ -1,10 +1,28 @@
+# import certifi
+# from pymongo import MongoClient
+#
+# # MongoDB Connection and Database
+# MONGO_URI = "mongodb+srv://ranvijay:ranvijay@cluster0.as3t4fm.mongodb.net/students"
+#
+# client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+#
+# db = client["students"]
+# collection = db["students"]
+
+
+import asyncio
 import certifi
-from pymongo import MongoClient
+import motor.motor_asyncio
 
 # MongoDB Connection and Database
 MONGO_URI = "mongodb+srv://ranvijay:ranvijay@cluster0.as3t4fm.mongodb.net/students"
 
-client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+# Create an event loop
+loop = asyncio.get_event_loop()
 
+# Create a Motor client
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI, tlsCAFile=certifi.where(), io_loop=loop)
+
+# Access the database
 db = client["students"]
 collection = db["students"]
