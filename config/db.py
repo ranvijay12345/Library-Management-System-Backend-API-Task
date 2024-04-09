2ce9d134e3e0fe8e55d3076a5d4d1d3a4d63eb58
Ranvijay Patel <ranvijayid76@gmail.com>
2024-04-09 20:24:44 +0530

    new change

--- config/db.py
+++ config/db.py
@@ -8,16 +8,3 @@ client = MongoClient(MONGO_URI)
 
 db = client["students"]
 collection = db["students"]
-
-
-# import asyncio
-# from motor.motor_asyncio import AsyncIOMotorClient
-#
-# # MongoDB Connection and Database
-# MONGO_URI = "mongodb+srv://ranvijay:ranvijay@cluster0.as3t4fm.mongodb.net/students"
-#
-# client = AsyncIOMotorClient(MONGO_URI)
-#
-# # Access the database
-# db = client["students"]
-# collection = db["students"]
