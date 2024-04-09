e77732011361a3f81b05c7eff053c610719b3c6c
Ranvijay Patel <ranvijayid76@gmail.com>
2024-04-09 17:18:57 +0530

    Modified db.py file

--- config/db.py
+++ config/db.py
@@ -1,8 +1,13 @@
+import ssl
+
 from pymongo import MongoClient
+import certifi
 
 # MongoDB Connection and Database
+ca = certifi.where()
 MONGO_URI = "mongodb+srv://ranvijay:ranvijay@cluster0.as3t4fm.mongodb.net/students"
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+
 db = client["students"]
 collection = db["students"]
